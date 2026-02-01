6bd9473667a110fac54b1006d417bbaaf2066c3a
mmerena <miroslaw.merena@gmail.com>
2026-02-01 20:35:28 +0100

    Update rce_prices_fetcher.py

--- rce_prices_fetcher.py
+++ rce_prices_fetcher.py
@@ -22,7 +22,7 @@ class RCEPricesFetcher(hass.Hass):
         default_logfile = os.path.join(log_dir, f"{base_name}.log")
         self.logger = self._setup_utf8_logger(self.args.get("logging", {}).get("file", default_logfile))
 
-        self.logger.info("=== Pobieranie RCE z PSE (APIv2) uruchomione ===")
+        self.logger.info("=== Aplikacja RCE API2 uruchomiona ===")
 
         # --- harmonogram dzienny ---
         schedule_cfg = self.args.get("schedule", {})
